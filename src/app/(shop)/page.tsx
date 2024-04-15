b6f945f71e7fdfe8adfb2d2e0da28e3d02552f66
Joako <joako12409@gmail.com>
2024-04-15 18:01:34 -0300

    Component Title.

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,12 +1,17 @@
 import { titleFont } from "@/config/fonts";
 import Image from "next/image";
+import {Title} from "@/components";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>Hola Mundo</h1>
-      <h1 className={`${titleFont.className} font-bold` }>Hola Mundo</h1>
-      <h1 className={`${titleFont.className}` }>Hola Mundo</h1>
-    </div>
+    <>
+    <Title
+    title="Tienda"
+    subtitle="Todos los productos"
+    className="mb-2"
+    />
+
+    
+    </>
   );
 }
