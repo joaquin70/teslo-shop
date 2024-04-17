d92975ad325c493423d740f7f10ccce359b0f422
Joako <joako12409@gmail.com>
2024-04-17 12:11:43 -0300

    Lateral Menu.

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,4 +7,5 @@ export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
 export * from "./ui/top-menu/TopMenu";
 export * from "./ui/not-found/PageNotFound";
+export * from "./ui/sidebar/Sidebar";
 export * from "./ui/title/Title";
